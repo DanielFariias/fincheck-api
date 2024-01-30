da56ebf85c1350211513d87602db3402c1369ffb
DanielFariias <danielfariias15@gmail.com>
2024-01-30 11:25:39 -0300

    Feat: Create database structure

---
+++ prisma/migrations/20240130142415_change_transaction_relationship/migration.sql
@@ -0,0 +1,26 @@
+-- DropForeignKey
+ALTER TABLE "categories" DROP CONSTRAINT "categories_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "transactions" DROP CONSTRAINT "transactions_bank_account_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "transactions" DROP CONSTRAINT "transactions_category_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "transactions" DROP CONSTRAINT "transactions_user_id_fkey";
+
+-- AlterTable
+ALTER TABLE "transactions" ALTER COLUMN "category_id" DROP NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "categories" ADD CONSTRAINT "categories_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transactions" ADD CONSTRAINT "transactions_bank_account_id_fkey" FOREIGN KEY ("bank_account_id") REFERENCES "bank_accounts"("id") ON DELETE CASCADE ON UPDATE CASCADE;
